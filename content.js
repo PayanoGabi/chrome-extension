383a492ba75fb60744f4c9ba6e70686d6c664520
Gabriela Payano <gabipayano@Gabrielas-MacBook-Air.local>
2022-09-13 19:27:21 -0700

    appending value

--- content.js
+++ content.js
@@ -8,49 +8,70 @@ var localKeys;
 chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
     
     localKeys = response;
+    arr = [];
+    
    // console.log(localKeys)
 
    Object.entries(localKeys).forEach((item, index) => {
 
-     
-    //console.log(Object.values(item))
 
-        if(Object.values(item)[0].indexOf("keyDB") > -1){
-            console.log(item[1])
-            console.log(item)
+        if(Object.values(item)[0].indexOf("keyDB") > -1 && currentDomain.indexOf('google.com')> -1){
+          console.log(item[1]) 
+          var stringKeys = " -" + item[1];
+            arr.push(" -" + item[1])
+            console.log(arr.toString())
+
+
+            var googleSearchBarCurrentValue = document.querySelector('[aria-label="Search"]');
+            googleSearchBarCurrentValue.value += arr.toString()
+
+
+
+            for (const [i, value] of arr.entries()) {
+                if (i === arr.length - 1) {
+                  // console.log(value)
+                   console.log(arr[i])
+
+
+
+                     // console.log(googleSearchBarCurrentValue.value)
+
+                      
+                    
+                     // googleSearchBarCurrentValue.setAttribute("values",arr[i])  //arr[i]
+
+
+                    //  console.log('yahoo.com')
+                    var googleSearchBarValue = document.querySelector('[aria-label="Search"]');
+
+                    
+                    console.log(googleSearchBarValue.value)
+                    
+      
+                    //   var googleSearchBarCurrentValue = document.querySelector('[aria-label="Search"]');
+
+                    //   console.log(googleSearchBarCurrentValue.value)
+      
+                
+                }
+            }
+
+        
+
+
+
+           // console.log(googleSearchBarValue)
+
+            googleSearchBarValue.setAttribute("value", googleSearchBarValue += stringKeys)
+            //document.querySelector("#tsf > div:nth-child(1) > div.A8SBwf > div.RNNXgb > button").click()
         }
+        
 
-    })
-
-
-
-
-    
+    })    
 
 })
 
 
-function hideKeywordsG(){
-    var googleSearchBar = document.querySelector('[aria-label="Search"]')
-      
-}
-function hideKeywordsY(){
-      
-}
-function hideKeywordsB(){
-      
-}
-
-  if(currentDomain.indexOf('google.com')> -1){
-      console.log('google.com')
-      var googleSearchBarValue = document.querySelector('[aria-label="Search"]').value;
-      hideKeywordsG()
-
-      console.log(googleSearchBarValue)
-      
-  }
-
- 
 
   if(currentDomain.indexOf('yahoo.com')> -1){
     console.log('yahoo.com')
